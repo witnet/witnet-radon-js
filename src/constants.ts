b41c6905083d4d65af690ce85bb7812a62f8646f
tommytrg <tomastrg22@gmail.com>
2020-07-10 16:29:36 +0200

    fix: update default script first type to be more generic

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ import { OperatorCode, OutputType } from './types'
 
 export const DEFAULT_OPERATOR = OperatorCode.ArrayCount
 export const DEFAULT_INPUT_TYPE = OutputType.Array
-export const DEFAULT_SCRIPT_FIRST_TYPE = OutputType.String
+export const DEFAULT_SCRIPT_FIRST_TYPE = OutputType.SubscriptOutput
