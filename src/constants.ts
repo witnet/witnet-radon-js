01b4a7c2f644022814d63f149878cf94d2f601ec
Guillermo Díaz <guillermo@otherplane.com>
2023-11-14 10:18:13 +0100

    feat: add support to HTTP/HEAD

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,7 @@ export const DEFAULT_SCRIPT_FIRST_TYPE = OutputType.String
 export const KIND_OPTIONS = [Kind.HttpGet, Kind.RNG, Kind.HttpPost]
 export const CONTENT_TYPE_OPTIONS = {
   [Kind.HttpGet]: 'JSON API',
+  [Kind.HttpHead]: 'JSON API',
   [Kind.HttpPost]: 'JSON API',
   [Kind.RNG]: 'Binary file',
 }
